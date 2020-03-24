e38f1f70d1bea520a338115662437cd24d9e27b4
SakhPrace <prace_life@mail.ru>
2020-03-24 03:56:57 +0300

    Initial commit

---
+++ src/main/java/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Controller {
+    Stage stageMain;
+    AnchorPane anchorPaneMain;
+    Scene sceneMain;
+}
