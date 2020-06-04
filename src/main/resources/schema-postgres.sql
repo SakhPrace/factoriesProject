8d82521bcc3feb79864dc297e8577f5b12bf9935
Nelly-Pak <nelly-loshadnitsa@yandex.ru>
2020-06-04 23:37:30 +0300

    Add id_transport to Ordering

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -2,4 +2,4 @@ CREATE SCHEMA if not exists public;
 CREATE TABLE IF NOT EXISTS factory(id serial PRIMARY KEY, name VARCHAR(255), exportid integer,password VARCHAR);
 CREATE TABLE IF NOT EXISTS product(id serial PRIMARY KEY, name VARCHAR(255), price_per_unit integer);
 CREATE TABLE IF NOT EXISTS transport_operator(id serial PRIMARY KEY, name VARCHAR(255), price_per_unit integer,password VARCHAR);
-CREATE TABLE IF NOT EXISTS ordering(id serial PRIMARY KEY, id_factory integer, id_product integer);
+CREATE TABLE IF NOT EXISTS ordering(id serial PRIMARY KEY, id_factory integer, id_product integer,id_transport integer);
