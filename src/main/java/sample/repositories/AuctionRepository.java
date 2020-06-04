7709ed7cec62701ae395214b52d80594f193606e
Nelly-Pak <nelly-loshadnitsa@yandex.ru>
2020-06-05 01:30:59 +0300

    Create Auction

--- src/main/java/sample/repositories/AuctionRepository.java
+++ src/main/java/sample/repositories/AuctionRepository.java
@@ -1,13 +1,4 @@
 package sample.repositories;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import sample.entities.AuctionEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface AuctionRepository extends CrudRepository<AuctionEntity, Integer>{
-    @Query("SELECT e FROM AuctionEntity e WHERE e.name = :username")
-    AuctionEntity getAuctionEntityByName(@Param("username") String username);
-
+public class AuctionRepository {
 }
--- src/main/java/sample/services/AuctionService.java
+++ src/main/java/sample/services/AuctionService.java
@@ -1,33 +1,4 @@
 package sample.services;
 
-import sample.entities.AuctionEntity;
-import sample.repositories.AuctionRepository;
-import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuctionService implements AuctionServiceInterface{
-
-    @Autowired(required = true)
-    private AuctionRepository repository;
-
-    @Override
-    public AuctionEntity findEntityById(Integer id) {
-        AuctionEntity auctionEntity = repository.findById(id).get();
-        return auctionEntity;
-    }
-
-    public AuctionEntity findEntityByName(String name) {
-        AuctionEntity auctionEntity = repository.getAuctionEntityByName(name);
-        return auctionEntity;
-    }
-
-    @Override
-    public void save(AuctionEntity auctionEntity) {
-        repository.save(auctionEntity);
-    }
+public class AuctionService {
 }
