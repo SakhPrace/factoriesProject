ee7010db786b7c82982ca08f3a8519dcdfbbc297
Nelly-Pak <nelly-loshadnitsa@yandex.ru>
2020-06-05 11:03:57 +0300

    Create links

--- src/main/resources/schema-maindb.sql
+++ src/main/resources/schema-maindb.sql
@@ -31,7 +31,9 @@ CREATE TABLE IF NOT EXISTS auction (
 id integer not null AUTO_INCREMENT PRIMARY KEY,
 id_order integer UNIQUE not null ,
 offered_price integer,
-id_transporter integer
+id_transporter integer,
+FOREIGN KEY (id_order) REFERENCES ordering(id),
+FOREIGN KEY (id_transporter) REFERENCES transport_operator(id)
 );
 
 CREATE TABLE IF NOT EXISTS factory_product (
@@ -40,3 +42,27 @@ product_id INT,
 FOREIGN KEY (factory_id) REFERENCES factory(id),
 FOREIGN KEY (product_id) REFERENCES product(id)
 );
+
+/*INSTALL SONAME 'ha_oqgraph';
+CREATE TABLE IF NOT EXISTS oq2_backing (
+origid INT UNSIGNED NOT NULL,
+destid INT UNSIGNED NOT NULL,
+weight DOUBLE NOT NULL,
+PRIMARY KEY (origid, destid),
+KEY (destid)
+);
+
+
+CREATE TABLE oq2_graph (
+latch VARCHAR(32) NULL,
+origid BIGINT UNSIGNED NULL,
+destid BIGINT UNSIGNED NULL,
+weight DOUBLE NULL,
+seq BIGINT UNSIGNED NULL,
+linkid BIGINT UNSIGNED NULL,
+KEY (latch, origid, destid) USING HASH,
+KEY (latch, destid, origid) USING HASH
+)
+
+ENGINE=OQGRAPH
+data_table='oq2_backing' origid='origid' destid='destid' weight='weight';*/
