8acf9f98cffa9f82dde5ae5b8d7867131881fcca
Nelly-Pak <nelly-loshdanitsa@yandex.ru>
2020-06-07 23:22:19 +0300

    Update tables

--- src/main/resources/data-maindb.sql
+++ src/main/resources/data-maindb.sql
@@ -13,12 +13,12 @@ INSERT INTO product(id, name, price_per_unit) VALUES(3, 'Electrical components',
 INSERT INTO product(id, name, price_per_unit) VALUES(4, 'Car', 100);
 INSERT INTO product(id, name, price_per_unit) VALUES(5, 'Furniture', 50);
 
-DELETE FROM maindb.transport_operator;
-INSERT INTO transport_operator(id, name, price_per_unit,password) VALUES(1, 'CCC1', 1, '$2y$16$rNQMZBVxYeKuqp.0qpujSOjO0tg6yzSME9DV4VxXbDu0Dom/YTH2a'); /*pT1*/
-INSERT INTO transport_operator(id, name, price_per_unit,password) VALUES(2, 'CCC2', 1, '$2y$16$.NP7.fYZSBCe9osNOIJjJOgsulr77qOle3ievVx7wne/FcgOIwA9q'); /*pT2*/
-INSERT INTO transport_operator(id, name, price_per_unit,password) VALUES(3, 'CCC3', 1, '$2y$16$QeN/My.qOPCJ.F0izfE7FefTka6C7rq8YixYqAXu9/uIUb0Jp0sn2'); /*pT3*/
-INSERT INTO transport_operator(id, name, price_per_unit,password) VALUES(4, 'CCC4', 1, '$2y$16$zcpleK/gEHiG5zhy1MHvZOAja3uBaWgdt1OsBCx2Qm2PC4.d1h4/2'); /*pT4*/
-INSERT INTO transport_operator(id, name, price_per_unit,password) VALUES(5, 'CCC5', 1, '$2y$16$IDh1wV7Fk..Yvx..UGXtdeRAtXz7dYXPBfGgZaiyJQ0gOFq5UAAwK'); /*pT5*/
+DELETE FROM maindb.transporter;
+INSERT INTO transporter(id, name, price_per_unit,password) VALUES(1, 'CCC1', 1, '$2y$16$rNQMZBVxYeKuqp.0qpujSOjO0tg6yzSME9DV4VxXbDu0Dom/YTH2a'); /*pT1*/
+INSERT INTO transporter(id, name, price_per_unit,password) VALUES(2, 'CCC2', 1, '$2y$16$.NP7.fYZSBCe9osNOIJjJOgsulr77qOle3ievVx7wne/FcgOIwA9q'); /*pT2*/
+INSERT INTO transporter(id, name, price_per_unit,password) VALUES(3, 'CCC3', 1, '$2y$16$QeN/My.qOPCJ.F0izfE7FefTka6C7rq8YixYqAXu9/uIUb0Jp0sn2'); /*pT3*/
+INSERT INTO transporter(id, name, price_per_unit,password) VALUES(4, 'CCC4', 1, '$2y$16$zcpleK/gEHiG5zhy1MHvZOAja3uBaWgdt1OsBCx2Qm2PC4.d1h4/2'); /*pT4*/
+INSERT INTO transporter(id, name, price_per_unit,password) VALUES(5, 'CCC5', 1, '$2y$16$IDh1wV7Fk..Yvx..UGXtdeRAtXz7dYXPBfGgZaiyJQ0gOFq5UAAwK'); /*pT5*/
 
 DELETE FROM maindb.ordering;
 INSERT INTO ordering(id, id_factory, id_product, id_transport, price) VALUES(1, 1, 2, 1, 3000);
--- src/main/resources/schema-maindb.sql
+++ src/main/resources/schema-maindb.sql
@@ -1,32 +1,36 @@
 CREATE SCHEMA if not exists public;
 CREATE DATABASE if not exists maindb;
+
 CREATE TABLE IF NOT EXISTS factory (
-id integer not null AUTO_INCREMENT PRIMARY KEY,
+id int not null AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(255),
-exportid integer,
+exportid int,
 password VARCHAR(255)
 );
+
 CREATE TABLE IF NOT EXISTS product (
-id integer not null AUTO_INCREMENT PRIMARY KEY,
+id int not null AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(255),
 price_per_unit integer
 );
 
-CREATE TABLE IF NOT EXISTS transport_operator (
-id integer not null AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS transporter (
+id int not null AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(255),
 price_per_unit integer,
 password VARCHAR(255)
 );
+
 CREATE TABLE IF NOT EXISTS ordering (
-id integer not null AUTO_INCREMENT PRIMARY KEY,
-id_factory integer,
-id_product integer,
-id_transport integer,
-price integer,
+id int not null AUTO_INCREMENT PRIMARY KEY,
+id_factory int,
+id_product int,
+id_transport int,
+price int,
+accepted boolean,
 FOREIGN KEY (id_factory) REFERENCES factory(id),
 FOREIGN KEY (id_product) REFERENCES product(id),
-FOREIGN KEY (id_transport) REFERENCES transport_operator(id)
+FOREIGN KEY (id_transport) REFERENCES transporter(id)
 );
 
 CREATE TABLE IF NOT EXISTS factory_product (
@@ -34,26 +38,6 @@ factory_id INT PRIMARY KEY,
 product_id INT,
 FOREIGN KEY (factory_id) REFERENCES factory(id),
 FOREIGN KEY (product_id) REFERENCES product(id)
-);
-CREATE TABLE IF NOT EXISTS auction (
-id integer not null AUTO_INCREMENT PRIMARY KEY,
-id_order integer UNIQUE not null ,
-offered_price integer,
-id_transporter integer,
-FOREIGN KEY (id_order) REFERENCES ordering(id),
-FOREIGN KEY (id_transporter) REFERENCES transport_operator(id)
-);
-CREATE TABLE IF NOT EXISTS order_auction (
-order_id INT PRIMARY KEY,
-auction_id INT,
-FOREIGN KEY (order_id) REFERENCES factory(id),
-FOREIGN KEY (auction_id) REFERENCES auction(id)
-);
-CREATE TABLE IF NOT EXISTS transporter_auction (
-transporter_id INT PRIMARY KEY,
-auction_id INT,
-FOREIGN KEY (transporter_id) REFERENCES transport_operator(id),
-FOREIGN KEY (auction_id) REFERENCES auction(id)
 );
 
 CREATE TABLE IF NOT EXISTS roads_backing (
